451ec3f9653cbba385d672d6d5d2195d29875bd6
JL <julien.leuenberger@epfl.ch>
2024-01-06 18:04:01 +0100

    Added update field to characters

--- app/app/models.py
+++ app/app/models.py
@@ -15,6 +15,7 @@ class Character(models.Model):
     name = models.CharField(max_length=20)
     icon=models.URLField(max_length=100, blank=True, null=True)
     owner = models.ForeignKey(Player, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f"[{self.owner}] {self.name}"
 
