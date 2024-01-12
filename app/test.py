d73460ef1a2615ec20f3c766f2b247004f9f4c51
JL <julien.leuenberger@outlook.com>
2024-01-12 16:23:06 +0100

    Added tests for "char" page & API

--- app/test.py
+++ app/test.py
@@ -5,8 +5,6 @@ import json
 import unittest
 
 TARGET_DOMAIN = "http://127.0.0.1:8000"
-TARGET_URL = TARGET_DOMAIN + "/uid/703047530"
-TARGET_URL_API = TARGET_DOMAIN + "/api/703047530"
 TIME_TO_WAIT = 5
 
 class DjangoServerTest(unittest.TestCase):
@@ -28,19 +26,31 @@ class DjangoServerTest(unittest.TestCase):
         url = TARGET_DOMAIN
         response = requests.get(url)
         self.response = response
-        self.assertEqual(response.status_code, 200, "Failed to make API request")
+        self.assertEqual(response.status_code, 200, "Failed to access the website")
 
     def test_player_access(self):
-        url = TARGET_URL
+        url = f"{TARGET_DOMAIN}/uid/703047530"
         response = requests.get(url)
         self.response = response
-        self.assertEqual(response.status_code, 200, "Failed to make API request")
+        self.assertEqual(response.status_code, 200, "Failed to access a player's page")
 
-    def test_api(self):
-        url = TARGET_URL_API
+    def test_player_api(self):
+        url = f"{TARGET_DOMAIN}/api/703047530"
         response = requests.get(url)
         json_data = response.json() if response.status_code == 200 else {}
         self.assertIn("uid", json_data, "'uid' field not found in the JSON response")
+
+    def test_character_access(self):
+        url = f"{TARGET_DOMAIN}/char/furina"
+        response = requests.get(url)
+        self.response = response
+        self.assertEqual(response.status_code, 200, "Failed to access a character's page")
+
+    def test_character_api(self):
+        url = f"{TARGET_DOMAIN}/api/furina"
+        response = requests.get(url)
+        json_data = response.json() if response.status_code == 200 else {}
+        self.assertIn("name", json_data, "'name' field not found in the JSON response")
 
 if __name__ == "__main__":
     unittest.main()
